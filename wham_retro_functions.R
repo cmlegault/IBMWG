1e991c5cc12d9134c4abee2e8615eea3e0320dbb
Tim Miller <timothyjasonmiller@gmail.com>
2020-06-16 13:31:21 -0400

    fix ramp and add change_Catch_sim, chanage_q_sim functions

--- wham_retro_functions.R
+++ wham_retro_functions.R
@@ -1,25 +1,28 @@
 #functions to make retro patterns
 
-change_q_sim = function(sim = NULL, new_q = c(0.4,0.8), year_change = 2000, years = 1970:2019)
+change_q_sim = function(sim = NULL, q_ratio = 1/3, n_ramp_years = 1, year_change = 2009, years = 1970:2019, change_proj = TRUE)
 {
   #proportions at age shouldn't be affected
   if(is.null(sim)) stop("need sim argument with simulated data")
-  if(length(new_q) != length(sim$q)) stop("new qs supplied do not match the number of indices")
-  sim$agg_indices[which(input$years<year_change),] = t(ratios * t(sim$agg_indices[which(input$years<year_change),]))
-  keep_I = sim$keep_I
-  keep_I[which(years<year_change),] = NA
-  sim$obsvec[keep_I+1] = sim$agg_indices[which(years<year_change),]
+  ramp_ratio = seq(1, q_ratio, length.out = n_ramp_years)
+  rampyears = year_change - (n_ramp_years-1):0 - min(years) + 1
+  sim$agg_indices[rampyears,] = ramp_ratio * sim$agg_indices[rampyears,]
+  sim$agg_indices[years>year_change,] = q_ratio * sim$agg_indices[years>year_change,]
+  keep_I = sim$keep_I[which(years>(year_change - n_ramp_years)),]
+  sim$obsvec[keep_I+1] = log(sim$agg_indices[which(years>(year_change - n_ramp_years)),])
   return(sim)
 }
 
-change_catch_sim = function(sim = NULL, catch_ratio = 1.5, year_change = 2000, years = 1970:2019)
+change_catch_sim = function(sim = NULL, catch_ratio = 1/3, n_ramp_years = 1, year_change = 2009, years = 1970:2019)
 {
   #proportions at age shouldn't be affected
   if(is.null(sim)) stop("need sim argument with simulated data")
-  sim$agg_catch[which(input$years<year_change),] = catch_ratio * sim$agg_catch[which(input$years<year_change),]
-  keep_C = sim$keep_C
-  keep_C[which(years<year_change),] = NA
-  sim$obsvec[keep_C+1] = sim$agg_catch[which(years<year_change),]
+  ramp_ratio = seq(1, catch_ratio, length.out = n_ramp_years)
+  rampyears = year_change - (n_ramp_years-1):0 - min(years) + 1
+  sim$agg_catch[rampyears,] = ramp_ratio * sim$agg_catch[rampyears,]
+  sim$agg_catch[years>year_change,] = catch_ratio * sim$agg_catch[years>year_change,]
+  keep_C = sim$keep_C[which(years>(year_change - n_ramp_years)),]
+  sim$obsvec[keep_C+1] = log(sim$agg_catch[which(years>(year_change - n_ramp_years)),])
   return(sim)
 }
 
@@ -30,10 +33,9 @@ change_M_om = function(wham_input = NULL, M_new_ratio = 3, n_ramp_years = 10, ye
   #if(length(new_M) != wham_input$data$n_ages) stop("new Ms supplied do not match the number of ages")
   logM = logMout = t(wham_input$par$M0 + wham_input$par$M_a + t(wham_input$par$M_re))
   ramp_ratio = seq(1, M_new_ratio, length.out = n_ramp_years)
-  rampyears = wham_input$years==year_change - (n_ramp_years-1):0
+  rampyears = year_change - (n_ramp_years-1):0 - min(wham_input$years) + 1
   logMout[rampyears,] = logMout[rampyears,] + log(ramp_ratio)
   logMout[wham_input$years>year_change,] = logMout[wham_input$years>year_change,] + log(M_new_ratio) 
-  print(exp(logMout))
   Mre = logMout - logM
   #print(Mre)  
   wham_input$par$M_re = Mre
