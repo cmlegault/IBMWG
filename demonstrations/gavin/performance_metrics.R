085adffef7920edb76187075d16066e7f3f6551d
gavinfay <gfay42@gmail.com>
2020-07-01 22:09:44 -0400

    fixes ssb performance metrics

--- demonstrations/gavin/performance_metrics.R
+++ demonstrations/gavin/performance_metrics.R
@@ -10,12 +10,19 @@ get_ssb_metrics <- function(ssb = NULL, refpts = NULL, nprojyrs = 40) {
   longyrs <- (fyear+20):length(ssb)
   projyrs <- fyear:length(ssb)
   metrics <- list(
-    is_less_01_bmsy = ifelse(any(ssb[projyrs]<0.1*SSBlim),1,0),
-    is_less_05_bmsy = ifelse(any(ssb[projyrs]<0.5*SSBlim),1,0),
-    is_ge_bmsy = ifelse(any(ssb[projyrs]>=SSBlim),1,0),
-    n_less_01_bmsy = length(which(ssb[projyrs]<0.1*SSBlim)),
-    n_less_05_bmsy = length(which(ssb[projyrs]<0.5*SSBlim)),
-    n_ge_bmsy = length(which(ssb[projyrs]>=SSBlim))
+    s_is_less_01_bmsy = ifelse(any(ssb[shortyrs]<0.1*SSBlim),1,0),
+    s_is_less_05_bmsy = ifelse(any(ssb[shortyrs]<0.5*SSBlim),1,0),
+    s_is_ge_bmsy = ifelse(any(ssb[shortyrs]>=SSBlim),1,0),
+    s_n_less_01_bmsy = length(which(ssb[shortyrs]<0.1*SSBlim)),
+    s_n_less_05_bmsy = length(which(ssb[shortyrs]<0.5*SSBlim)),
+    s_n_ge_bmsy = length(which(ssb[shortyrs]>=SSBlim))
+
+    l_is_less_01_bmsy = ifelse(any(ssb[longyrs]<0.1*SSBlim),1,0),
+    l_is_less_05_bmsy = ifelse(any(ssb[longyrs]<0.5*SSBlim),1,0),
+    l_is_ge_bmsy = ifelse(any(ssb[longyrs]>=SSBlim),1,0),
+    l_n_less_01_bmsy = length(which(ssb[longyrs]<0.1*SSBlim)),
+    l_n_less_05_bmsy = length(which(ssb[longyrs]<0.5*SSBlim)),
+    l_n_ge_bmsy = length(which(ssb[longyrs]>=SSBlim))
   )
   return(metrics)
 }
