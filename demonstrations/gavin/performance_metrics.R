d905c79894ac46e8286d04fb88f8aa7feb98c561
gavinfay <gfay42@gmail.com>
2020-07-23 11:28:48 -0400

    updates imgs & performance metrics

--- demonstrations/gavin/performance_metrics.R
+++ demonstrations/gavin/performance_metrics.R
@@ -15,8 +15,7 @@ get_ssb_metrics <- function(ssb = NULL, refpts = NULL, nprojyrs = 40) {
     s_is_ge_bmsy = ifelse(any(ssb[shortyrs]>=SSBlim),1,0),
     s_n_less_01_bmsy = length(which(ssb[shortyrs]<0.1*SSBlim)),
     s_n_less_05_bmsy = length(which(ssb[shortyrs]<0.5*SSBlim)),
-    s_n_ge_bmsy = length(which(ssb[shortyrs]>=SSBlim))
-
+    s_n_ge_bmsy = length(which(ssb[shortyrs]>=SSBlim)),
     l_is_less_01_bmsy = ifelse(any(ssb[longyrs]<0.1*SSBlim),1,0),
     l_is_less_05_bmsy = ifelse(any(ssb[longyrs]<0.5*SSBlim),1,0),
     l_is_ge_bmsy = ifelse(any(ssb[longyrs]>=SSBlim),1,0),
