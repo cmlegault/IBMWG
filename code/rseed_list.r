e48de152769590b849abc48babd3f1ac9e8e9bfb
cmlegault <chris.legault@noaa.gov>
2020-09-15 07:38:01 -0400

    modify RNG seeds code to output to settings directory

--- code/rseed_list.r
+++ code/rseed_list.r
@@ -4,9 +4,10 @@
 # 2017 June 01
 #
 
-rm(list=ls(all=TRUE))
+# see https://www.tidyverse.org/blog/2017/12/workflow-vs-script/
+# rm(list=ls(all=TRUE)) 
 
 set.seed(799291)
 nseeds <- 1000
 r.seed.set <- trunc(1e7*runif(n=nseeds),7) + trunc(1e3*runif(n=nseeds),3)
-write.table(r.seed.set, file="RNG.seeds.csv", quote=F, row.names=F, col.names=F, sep=",")
+write.table(r.seed.set, file="settings/RNG.seeds.csv", quote=F, row.names=F, col.names=F, sep=",")
