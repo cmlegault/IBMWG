87f8eb2cf40f70c2f5b1b405ac873134d1809092
gavinfay <gfay42@gmail.com>
2020-10-07 17:51:08 -0400

    adds a "reset_gaps" script that can be used to reset the progress table for simulations that didn't run properly.

---
+++ code/reset_gaps.R
@@ -0,0 +1,60 @@
+# Script to change the values in progress table that got run, but where the simulations didn't work
+# GF 2020/10/07
+library(tidyverse)
+
+# load in progress table
+progress <- readRDS(file = "settings/progress_table.rds")
+
+#copy the progress table for my sanity's sake
+today = format(Sys.time(),"%Y-%m-%d-%H-%M-%S") #for actual date
+outfile = paste0("progress_table_",str_remove_all(today,"-"),".rds")
+system(paste0("cp settings/progress_table.rds settings/",outfile))
+
+
+
+#load the results (these are simulations that worked)
+# currently reading from Chris' demo perform metrics file. 
+# This won't have the full set of results that have been done yet SO don't use this to rewrite progress table.rds at this time
+# (the line of code for that hasn't been written here purposefully to hedge against that)
+
+# ideally I  think rather than reading in the performance metrics table one would read in the results fiels and check,
+# but that's essentially what is happening here.
+mse_perform <- readRDS(file = "demonstrations/chris/demo_plots/demo-perform-metrics.rds")
+mse_perform
+
+
+ # look at rows that there are results for
+results_for <- progress %>% 
+  slice(which(rowid %in% mse_perform$rowid == TRUE)) %>% 
+  I()
+results_for %>% filter(is.na(uploaded))
+
+# so there are some runs that there are results for but 
+# for which the progress table does not have an 'uploaded flag'
+
+#reset the progress table with these
+progress$uploaded[progress$rowid %in% results_for$rowid] <- TRUE
+
+
+# now look at rruns that need to be reset (i.e. uploaded = TRUE but they didn't work)
+
+# find the progress that have been done
+done <- progress %>% 
+  #filter(is.na(user)) %>%
+  #filter(IBM != "ensemble") %>%   ## this removes the ensembles 
+  #slice(1:nsim) %>% select(rowid) %>% t() %>% as.numeric() %>% 
+  drop_na() %>% 
+  I()
+
+to_reset <- done %>% 
+  slice(which(rowid %in% mse_perform$rowid == FALSE)) %>% 
+  I()
+#results_for %>% filter(is.na(uploaded))
+
+#reset the progress table
+progress$uploaded[progress$rowid %in% to_reset$rowid] <- NA
+progress$user[progress$rowid %in% to_reset$rowid] <- NA
+progress$date_run[progress$rowid %in% to_reset$rowid] <- NA
+
+# add the line to save the new revised progress table here
+
