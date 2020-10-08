e1ef2d7d0ee9b0c021ef2c5f0581345c7cdddd53
Gavin Fay <gfay42@gmail.com>
2020-10-08 14:25:09 -0400

    updates progress table

--- run_script.R
+++ run_script.R
@@ -5,4 +5,4 @@ purrr::map(rscripts, source)
 future::plan(future::multisession)
 
 # run it
-do_mse(nsim = args[1], user = "GF", write_to_google = FALSE)
+do_mse(nsim = args[1], user = args[2], write_to_google = FALSE)
