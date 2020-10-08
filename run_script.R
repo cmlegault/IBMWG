d635825727194501aaa8ff9745c9a9fab6b51f88
liz.brooks <liz.brooks@noaa.gov>
2020-10-08 16:27:56 -0400

    updates progress table

--- run_script.R
+++ run_script.R
@@ -3,5 +3,13 @@ rscripts <- c("code/do_mse.R")
 purrr::map(rscripts, source)
 future::plan(future::multisession)
 
-# run it
-do_mse(nsim = 2000, user = "GF", write_to_google = FALSE)
+# run it inside a loop
+nloop <- 2
+
+for (iloop in 1:nloop) {
+  
+print(paste("Starting iteration ", iloop, ", for do_mse.R", sep=""))
+do_mse(nsim = 2*224, user = "LB", write_to_google = FALSE)
+
+}
+  
