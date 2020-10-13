c6b014420eb80cd475fdb7158a6dd9cd1226f11d
gavinfay <gfay42@gmail.com>
2020-10-13 10:42:29 -0400

    updates progress table

--- run_script.R
+++ run_script.R
@@ -2,7 +2,8 @@ args = commandArgs(trailingOnly=TRUE)
 
 rscripts <- c("code/do_mse.R")
 purrr::map(rscripts, source)
-future::plan(future::multisession)
+#future::plan(future::multisession)
+future::plan(future.callr::callr)
 
 # run it
 do_mse(nsim = args[1], user = args[2], write_to_google = FALSE)
