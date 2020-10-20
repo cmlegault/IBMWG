b0e7211e16544d9bca938eab51844931a8fc7cc2
cmlegault <chris.legault@noaa.gov>
2020-10-20 14:11:06 -0400

    add score plot

--- demonstrations/chris/demo_plots/demo_scores.R
+++ demonstrations/chris/demo_plots/demo_scores.R
@@ -51,3 +51,34 @@ catch_scores <- calc_scores(catch_mean_by_scenario, catch_bigger_better) %>%
 all_scores <- rbind(ssb_scores, f_scores, catch_scores)
 
 write.csv(all_scores, file="demonstrations/chris/demo_plots/all_scores.csv")
+
+get_mean_scores <- function(mywide){
+  mymeans <- mywide %>%
+    pivot_longer(cols = !metric, names_to = "IBM", values_to = "score") %>%
+    group_by(IBM) %>%
+    summarize(meanscore = mean(score))
+  return(mymeans)
+}
+
+ssb_means <- get_mean_scores(ssb_scores) %>%
+  rename("ssb" = "meanscore")
+
+f_means <- get_mean_scores(f_scores) %>%
+  rename("f" = "meanscore")
+
+catch_means <- get_mean_scores(catch_scores) %>%
+  rename("catch" = "meanscore")
+
+all_means <- inner_join(ssb_means, f_means, by = "IBM") %>%
+  inner_join(catch_means, by = "IBM") %>%
+  mutate(mean_of_means = (ssb + f + catch) / 3) %>%
+  pivot_longer(cols = !IBM, names_to = "source", values_to = "score") 
+
+score_plot <- ggplot(all_means, aes(x=reorder(IBM, score), y=score)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  facet_wrap(~source) +
+  labs(x="IBM", y="Score (bigger is better)") +
+  theme_bw()
+print(score_plot)
+ggsave(filename = "demonstrations/chris/demo_plots/score_plot.png")
