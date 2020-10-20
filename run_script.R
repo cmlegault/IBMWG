79fdd02a56c7377b0b25e1a0e7d94e9ef0d28eff
liz.brooks <liz.brooks@noaa.gov>
2020-10-20 17:54:42 -0400

    updates progress table

--- run_script.R
+++ run_script.R
@@ -6,4 +6,4 @@ purrr::map(rscripts, source)
 future::plan(future.callr::callr)
 
 # run it
-do_mse(nsim = 25, user = "LB", write_to_google = FALSE)
+do_mse(nsim = 50, user = "LB", write_to_google = FALSE)
