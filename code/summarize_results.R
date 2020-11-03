5774911b92f6352b5596adb00313e535d464deeb
cmlegault <chris.legault@noaa.gov>
2020-11-03 07:56:36 -0500

    allow for user defined folder of results

--- code/summarize_results.R
+++ code/summarize_results.R
@@ -1,89 +1,125 @@
-# reads in results files from googledrive and compiles
-# probably want an intermediate save to file after extracting performance metrics
+# reads in results files from user defined location
+# an intermediate save to results directory after extracting performance metrics
 # then for viz/analysis can just work with that tibble
 
+# user note: be sure that mypath and newfiles are defined correctly
+
 ## install needed libraries & scripts
-#library(wham)
 library(tidyverse)
-#library(furrr)
-library(googledrive)
 
 rscripts <- c("code/performance_metrics.R")
 map(rscripts, source)
 
-# 
-# # download the output & read them in
-# xx <- drive_ls("ibm-test/output")
-# map(xx$name,drive_download, overwrite = TRUE)
-# mse_output <- map_df(xx$name,readRDS)
-# 
-#mse_output <- readRDS("dummy_output.rds")
-files <- dir(path = "output",
-             pattern = "*.rds",
-             full.names = TRUE)
-files <- str_subset(files, "output/mse-")
+# User defined path where output rds files are located
+#mypath <- "/net/home0/pdy/lbrooks/Git/output_oct14_IBMWG"
+#mypath <- "/net/home4/clegault/IBMWG/output"
+myfiles <- list.files(mypath)
+myfiles <- str_subset(myfiles, "mse-")
+myfiles
 
+# which files are read (will skip files already in database)
+newfiles <- myfiles # can subset using brackets for testing, e.g., myfiles[1:3]
+nfiles <- length(newfiles)
 
-mse_output <- map_dfr(files, readRDS) %>% 
-  filter(map_lgl(wham, ~(.x %>% pluck("result", 1, 1) %>% is.na==FALSE))) %>% 
-  I()
+# defines names of files that are saved
+mainfile_name <- "results/perform-metrics.rds"
+dbfile_name <- "results/dbfile.rds"
+dbrowid_name <- "results/dbrowid/rds"
 
-#mse_output <- readRDS("output/mse-CML-20200930113704.rds")
-#mse_output <- readRDS("~/Dropbox/mse_test_out.rds")
-
-# calculate performance metrics
-mse_results <- mse_output %>% 
-  mutate(finished = map(wham, "result"),
-         finished = map(finished, "finished"),
-         fin2 = Reduce(c, finished), 
-         size = map_dbl(wham, object.size),
-         om_ssb = map(wham,
-                      ~pluck(.x$result$true_sim$SSB)),
-         catch = map(wham, 
-                     ~pluck(.x$result$true_sim$pred_catch)), #true catch (without observation error)
-         frate = map(wham, 
-                     ~pluck(.x$result$true_sim$Fbar)), #Fbar is full F because it is averaged over just the plus group
-         catch = map(catch, na_if, y = "NaN"),
-         om_ssb = map(om_ssb, na_if, y = "NaN"),
-         frate = map(frate, na_if, y = "NaN"),
-         refpts = map(wham,
-                     ~pluck(.x$result$refpts)),
-         nprojyrs = map(specs, "nprojyrs"),
-         ssb_metrics = pmap(list(om_ssb, refpts, nprojyrs), get_ssb_metrics),
-         catch_metrics = pmap(list(catch, refpts, nprojyrs), get_catch_metrics),
-         f_metrics = pmap(list(frate, refpts, nprojyrs), get_F_metrics)
-         ) %>% 
-  slice(which(!(fin2 < "2020-10-12" & iscen > 112))) %>%   #filter the bogus catchmult 0.75 runs
-  select(rowid, iscen, isim, ssb_metrics, catch_metrics, f_metrics) %>% 
-  I()
-
-#save the performance metrics object
-saveRDS(mse_results, file = "settings/perform-metrics.rds")
-
-
-#`mse_results` is a tibble containing the lists of SSB & catch performance metrics
-
-#pull out the ssb metrics
-ssb_results <- mse_results %>% 
-  #select(rowid, ssb_metrics) %>% 
-  select(iscen, isim, ssb_metrics) %>% 
-  mutate(ssb_metrics = map(ssb_metrics, enframe)) %>% 
-  unnest(cols = c(ssb_metrics)) %>% 
-  mutate(value = map_dbl(value, I)) %>% 
-  rename(metric = name) %>% 
-  I()
-ssb_results
-#```
-
-#summarize across simulations by scenario
-#25%, 50%, 75% quantiles
-quibble <- function(x, q = c(0.25, 0.5, 0.75)) {
-  tibble(x = quantile(x, q, na.rm = TRUE), q = q)
+# databases of file locations and which simulations run in each file
+if(file.exists(dbfile_name)){
+  dbfile_start <- readRDS(dbfile_name)
+}else{
+  dbfile_start <- tibble(link = character(),
+                         file = character(),
+                         location = character())
 }
 
-ssb_summary <- ssb_results %>% 
-  group_by(metric, iscen) %>% 
-  summarise(y = list(quibble(value, c(0.25, 0.5, 0.75)))) %>% 
-  tidyr::unnest(y) %>% 
-  I()
-ssb_summary
+if(file.exists(dbrowid_name)){
+  dbrowid_start <- readRDS(dbrowid_name)
+}else{
+  dbrowid_start <- tibble(link = character(),
+                          rowid = integer(),
+                          iscen = integer(),
+                          isim = integer())
+}
+
+myresults <- NULL # so that first loop gets saved
+
+for (i in 1:nfiles){
+  if (!(newfiles[i] %in% dbfile_start$file)){ # skip if already read this file
+    thisrds <- readRDS(file.path(mypath, newfiles[i]))
+    if (dim(thisrds)[1] > 0){ # skip if nothing in the file
+      mse_output <- thisrds %>%
+        filter(map_lgl(wham, ~(.x %>% pluck("result", 1, 1) %>% is.na==FALSE)))
+      
+      # calculate performance metrics
+      mse_results <- mse_output %>% 
+        mutate(finished = map(wham, "result"),
+               finished = map(finished, "finished"),
+               fin2 = Reduce(c, finished), 
+               size = map_dbl(wham, object.size),
+               om_ssb = map(wham,
+                            ~pluck(.x$result$true_sim$SSB)),
+               catch = map(wham, 
+                           ~pluck(.x$result$true_sim$pred_catch)), #true catch (without observation error)
+               frate = map(wham, 
+                           ~pluck(.x$result$true_sim$Fbar)), #Fbar is full F because it is averaged over just the plus group
+               catch = map(catch, na_if, y = "NaN"),
+               om_ssb = map(om_ssb, na_if, y = "NaN"),
+               frate = map(frate, na_if, y = "NaN"),
+               refpts = map(wham,
+                            ~pluck(.x$result$refpts)),
+               nprojyrs = map(specs, "nprojyrs"),
+               ssb_metrics = pmap(list(om_ssb, refpts, nprojyrs), get_ssb_metrics),
+               catch_metrics = pmap(list(catch, refpts, nprojyrs), get_catch_metrics),
+               f_metrics = pmap(list(frate, refpts, nprojyrs), get_F_metrics)
+        ) %>% 
+        slice(which(!(fin2 < "2020-10-12" & iscen > 112))) %>%  #filter out the bogus catchmult 0.75 runs
+        select(rowid, iscen, isim, ssb_metrics, catch_metrics, f_metrics) %>% 
+        I()
+      
+      # save file info
+      today = format(Sys.time(),"%Y-%m-%d-%H-%M-%S") #for actual date
+      mylink <- paste0(today, "-", i) # ensures unique id
+      thisfile <- tibble(link = mylink,
+                         file = newfiles[i],
+                         location = mypath)
+      thisrowid <- tibble(link = mylink,
+                          rowid = mse_results$rowid,
+                          iscen = mse_results$iscen,
+                          isim = mse_results$isim)
+      
+      if (is.null(myresults)){
+        myresults <- mse_results
+        dbfile <- thisfile
+        dbrowid <- thisrowid
+      }else{
+        myresults <- rbind(myresults, mse_results)
+        dbfile <- rbind(dbfile, thisfile)
+        dbrowid <- rbind(dbrowid, thisrowid)
+      }
+      # uncomment following line to watch progress in real time
+      #print(paste("file", newfiles[i], i, "of", nfiles, "had", dim(mse_results)[1], "successful simulations"))
+    }
+  }
+}
+
+# get results done previously
+if (file.exists(mainfile_name)){
+  mse_results_start <- readRDS(mainfile_name)
+  combined_results <- rbind(mse_results_start, myresults)
+}else{
+  combined_results <- myresults
+}
+
+# add new files to previous files
+combined_dbfile <- rbind(dbfile_start, dbfile)
+combined_dbrowid <- rbind(dbrowid_start, dbrowid)
+
+#save the performance metrics object
+saveRDS(combined_results, file = mainfile_name)
+saveRDS(combined_dbfile, file = dbfile_name)
+saveRDS(combined_dbrowid, file = dbrowid_name)
+
+
