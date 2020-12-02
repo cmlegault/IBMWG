4e04be2e9a65efea6ab0931d1f360c3cfb2769a3
cmlegault <chris.legault@noaa.gov>
2020-12-02 16:52:29 -0500

    add survey selectivity plot

--- demonstrations/chris/basics/basics.R
+++ demonstrations/chris/basics/basics.R
@@ -23,3 +23,16 @@ waaplot <- ggplot(filter(datlong, metric == "Weight"), aes(x=Age, y=value)) +
   theme_bw()
 ggsave(filename = "demonstrations/chris/basics/waaplot.png", waaplot, width = 5, height = 4, units = "in")
 
+dat2 <- read.csv("demonstrations/chris/basics/surveysel.csv")
+dat2
+
+dat2long <- dat2 %>%
+  pivot_longer(cols = -1, names_to = "Survey", values_to = "value")
+dat2long
+
+survselplot <- ggplot(dat2long, aes(x=Age, y=value, color=Survey)) +
+  geom_line(lwd=2) +
+  scale_x_continuous(breaks = seq(2, 10, 2), labels = c("2", "4", "6", "8", "10+")) +
+  labs(y="Selectivity") +
+  theme_bw()
+ggsave(filename = "demonstrations/chris/basics/survselplot.png", survselplot, width = 4, height = 3, units = "in")
