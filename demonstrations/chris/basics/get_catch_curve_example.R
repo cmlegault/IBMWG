0aa8c33de16abc06060803e07a2913bb2dee97fe
cmlegault <chris.legault@noaa.gov>
2021-02-09 12:38:01 -0500

    replace PlanB with PBS

--- demonstrations/chris/basics/get_catch_curve_example.R
+++ demonstrations/chris/basics/get_catch_curve_example.R
@@ -26,7 +26,7 @@ defined <- mse_sim_setup %>%
   mutate(IBMlab = case_when(
     IBM == "true_Skate_CR" ~ "Skate",
     IBM == "M_CC" ~ paste("CC", CClab[M_CC_method], sep="-"),
-    IBM == "planBsmoothfxn" ~ "PlanB",
+    IBM == "planBsmoothfxn" ~ "PBS",
     IBM == "ExpandSurvey_modified" ~ paste("ES", EMlab[expand_method], sep="-"),
     IBM == "run.aim" ~ "AIM",
     IBM == "JoeDLM" ~ "DLM",
