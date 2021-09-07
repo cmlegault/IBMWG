9f32fc9e1e95a90a866d4d5fa7281d77603a6624
cmlegault <chris.legault@noaa.gov>
2021-09-07 16:33:12 -0400

    add error message from Shiny1 server

--- code/find_run_missing_sims.R
+++ code/find_run_missing_sims.R
@@ -1,8 +1,68 @@
 # find_run_missing_sims.R
 # find and run the four missing simulations to examine what went wrong
 
+library(wham)
 library(tidyverse)
+library(furrr)
 
+rscripts <- c("code/base_input.R",
+              "code/change_input.R",
+              "code/IBM_options.R",
+              #"code/performance_metrics.R",
+              "code/wham_mse_functions.R",
+              "code/wham_retro_functions.R")
+map(rscripts, source)
+my_future_options = future_options()
+my_future_options$globals = ls()
+my_future_options$packages <- c("wham",
+                                "tidyverse",
+                                "dlm",
+                                "RandomFieldsUtils")
+
+# get results and simulation set up information
+mse_results <- readRDS("results/perform-metrics_clean_full_DLM.rds")
+scaa_results <- readRDS("results/perform-metrics_full_scaa.rds") # note full
+mse_sim_setup <- readRDS("settings/mse_sim_setup.rds")
+input_setup <- readRDS(file = "settings/input_setup.rds")
+
+# remove duplicate runs
+dupes <- duplicated(mse_sim_setup[,-(1:2)])
+not_dupes <- mse_sim_setup$rowid[!dupes]
+
+# check number of simlations per scenario
+count_table <- mse_results %>%
+  group_by(iscen) %>%
+  summarise(n = length(unique(isim)))
+count_table$n
+
+### join with setup to figure out what's in each scenario
+Fhistlab <- c("F","O") # O = always overfishing, F = Fmsy in last year of base
+Sellab <- c("1", "2") # just whether 1 or 2 blocks for selectivity
+CMlab <- c("A", "R") # A = catch advice applied, R = reduced (mult=0.75)
+EMlab <- c("FSPR", "Fstable", "FM", "Frecent")
+CClab <- c("FSPR", NA, "FM")
+defined <- mse_sim_setup %>%
+  filter(rowid %in% not_dupes) %>% 
+  filter(isim == 1) %>%
+  select(iscen, specs) %>%
+  unnest(cols = specs) %>%
+  inner_join(count_table, by="iscen") %>%
+  mutate(IBMlab = case_when(
+    IBM == "true_Skate_CR" ~ "Skate",
+    IBM == "M_CC" ~ paste("CC", CClab[M_CC_method], sep="-"),
+    IBM == "planBsmoothfxn" ~ "PBS",
+    IBM == "ExpandSurvey_modified" ~ paste("ES", EMlab[expand_method], sep="-"),
+    IBM == "run.aim" ~ "AIM",
+    IBM == "JoeDLM" ~ "DLM",
+    IBM == "ensemble" ~ "Ensemble",
+    TRUE ~ IBM),
+    Scenlab = paste(substr(retro_type, 1, 1), Fhistlab[Fhist], Sellab[n_selblocks], ifelse(catch.mult == 1, CMlab[1], CMlab[2]), sep = ""))
+defined
+names(defined)
+unique(defined$IBMlab)
+unique(defined$Scenlab)
+
+# now find the bad runs
 bad1 <- defined %>%
   filter(IBMlab == "AIM",
          retro_type == "Catch",
@@ -66,14 +126,8 @@ fourbad <- data.frame(iscen = c(bad1$iscen, bad2$iscen, bad3$iscen, bad4$iscen),
                                bad4$missing_isim))
 fourbad
 
-# load in the scenario specifications
-mse_sim_setup <- readRDS(file = "settings/mse_sim_setup.rds")
-
-# load in the input objects
-input_setup <- readRDS(file = "settings/input_setup.rds")
-
 # select just the four bad runs
-to_do <- left_join(fourbad, mse_sim_setup, by = c("iscen", "isim")) %>% 
+todo <- left_join(fourbad, mse_sim_setup, by = c("iscen", "isim")) %>% 
   select(rowid) %>% t() %>% as.numeric() %>% 
   I()
 
@@ -92,10 +146,15 @@ safe_wham_mse <- purrr::safely(do_wham_mse_sim, otherwise = NA_real_)
 #profvis::profvis(
 #system.time(
 mse_output <- mse_sim_todo %>% 
-  mutate(wham = furrr::future_pmap(list(seed = seed, input = input),
-                                   safe_wham_mse, .options = my_future_options)) %>% 
+  #mutate(wham = furrr::future_pmap(list(seed = seed, input = input),
+  #                                 safe_wham_mse, .options = my_future_options)) %>% 
   # this is the regular purrr code for iterating over the simulations
-  #mutate(wham = purrr::pmap(list(seed = seed, input = input), do_wham_mse_sim))
+  mutate(wham = purrr::pmap(list(seed = seed, input = input), do_wham_mse_sim))
   select(-input) %>% 
   I()
 #) #ends profvis
+  
+# get the following Error message when run mse_output line:
+  Error: Problem with `mutate()` input `wham`.
+  x NA/NaN/Inf in 'x'
+  ℹ Input `wham` is `purrr::pmap(list(seed = seed, input = input), do_wham_mse_sim)`.
